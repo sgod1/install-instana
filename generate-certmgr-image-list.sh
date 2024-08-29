bf10b9ac4ceaae5a63d56241e862e1236976b3be
Simon Godik <simon@godik.com>
2024-08-29 10:26:32 -0700

    cert manager version update

--- generate-certmgr-image-list.sh
+++ generate-certmgr-image-list.sh
@@ -8,9 +8,9 @@ MIRROR_HOME=$(get_make_mirror_home)
 echo writing cert manager image list to $MIRROR_HOME/$CERT_MGR_IMAGE_LIST_FILE
 
 echo "
-quay.io/jetstack/cert-manager-cainjector:v1.7.1
-quay.io/jetstack/cert-manager-controller:v1.7.1
-quay.io/jetstack/cert-manager-webhook:v1.7.1
-quay.io/jetstack/cert-manager-acmesolver:v1.7.1
-quay.io/jetstack/cert-manager-ctl:v1.7.1
+quay.io/jetstack/cert-manager-cainjector:v1.13.2
+quay.io/jetstack/cert-manager-controller:v1.13.2
+quay.io/jetstack/cert-manager-webhook:v1.13.2
+quay.io/jetstack/cert-manager-acmesolver:v1.13.2
+quay.io/jetstack/cert-manager-ctl:v1.13.2
 " > $MIRROR_HOME/$CERT_MGR_IMAGE_LIST_FILE
