67f44e60d5d6805e8f4588df21a3142c63329b82
Simon Godik <simon@godik.com>
2025-01-09 08:47:53 +0000

    use data-image-env variables

--- 3-pull-datastore-charts.sh
+++ 3-pull-datastore-charts.sh
@@ -4,6 +4,8 @@ source ../instana.env
 
 source ./help-functions.sh
 
+source ./datastore-images.env
+
 CHART_HOME=$(get_make_chart_home)
 
 helm repo add instana https://helm.instana.io/artifactory/rel-helm-customer-virtual --username _ --password $DOWNLOAD_KEY
@@ -11,22 +13,22 @@ helm repo add instana https://helm.instana.io/artifactory/rel-helm-customer-virt
 helm repo update
 
 # zookeeper operator
-helm pull instana/zookeeper-operator --version=0.2.15 -d $CHART_HOME
+helm pull instana/zookeeper-operator --version=${ZOOKEEPER_OPERATOR_CHART_VERSION} -d $CHART_HOME
 
 # kafka operator
-helm pull instana/strimzi-kafka-operator --version=0.41.0 -d $CHART_HOME
+helm pull instana/strimzi-kafka-operator --version=${KAFKA_OPERATOR_CHART_VERSION} -d $CHART_HOME
 
 # elasticsearch
-helm pull instana/eck-operator --version=2.9.0 -d $CHART_HOME
+helm pull instana/eck-operator --version=${ELASTICSEARCH_OPERATOR_CHART_VERSION} -d $CHART_HOME
 
 # postgres
-helm pull instana/cloudnative-pg --version=0.20.0 -d $CHART_HOME
+helm pull instana/cloudnative-pg --version="${POSTGRES_OPERATOR_CHART_VERSION}" -d $CHART_HOME
 
 # cassandra
-helm pull instana/cass-operator --version=0.45.2 -d $CHART_HOME
+helm pull instana/cass-operator --version=${CASSANDRA_OPERATOR_CHART_VERSION} -d $CHART_HOME
 
 # clickhouse
-helm pull instana/ibm-clickhouse-operator --version=v0.1.2 -d $CHART_HOME
+helm pull instana/ibm-clickhouse-operator --version=${CLICKHOUSE_OPERATOR_CHART_VERSION} -d $CHART_HOME
 
 # beeinstana
 helm pull instana/beeinstana-operator --version=v1.58.0 -d $CHART_HOME
--- install-cassandra-operator.sh
+++ install-cassandra-operator.sh
@@ -17,7 +17,7 @@ custom_webhook_cert=${1:-$NO_CUSTOM_WEBHOOK_CERT}
 CHART_HOME=$(get_chart_home)
 MANIFEST_HOME=$(get_manifest_home)
 
-CHART=$CHART_HOME/cass-operator-0.45.2.tgz
+CHART=$CHART_HOME/cass-operator-${CASSANDRA_OPERATOR_CHART_VERSION}.tgz
 
 SCC=$MANIFEST_HOME/$MANIFEST_FILENAME_CASSANDRA_SCC
 
@@ -99,16 +99,19 @@ echo ""
 
 set -x
 
+cassandra_operator_img_repo=`echo $CASSANDRA_OPERATOR_IMG | cut -d : -f 1 -`
+cassandra_operator_img_tag=`echo $CASSANDRA_OPERATOR_IMG | cut -d : -f 2 -`
+
 helm install cassandra-operator -n instana-cassandra $CHART \
    $set_admission_webhook $set_custom_webhook_cert \
    --set securityContext.runAsNonRoot=true \
    --set securityContext.runAsUser=999 \
    --set securityContext.runAsGroup=999 \
    --set image.registry=$PRIVATE_REGISTRY \
-   --set image.repository="$CASSANDRA_OPERATOR_IMG_REPO" \
-   --set image.tag="$CASSANDRA_OPERATOR_IMG_TAG" \
+   --set image.repository="$cassandra_operator_img_repo" \
+   --set image.tag="$cassandra_operator_img_tag" \
    --set imagePullSecrets[0].name="instana-registry" \
-   --set appVersion="1.18.2" \
+   --set appVersion="$CASSANDRA_OPERATOR_CHART_APP_VERSION" \
    --set imageConfig.systemLogger="$PRIVATE_REGISTRY/$CASSANDRA_SYSTEM_LOGGER_IMG" \
    --set imageConfig.k8ssandraClient="$PRIVATE_REGISTRY/$CASSANDRA_K8S_CLIENT_IMG" \
    --set imageConfig.configBuilder="$PRIVATE_REGISTRY/$CASSANDRA_CONFIG_BUILDER_IMG"
