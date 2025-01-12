166e9a23f706725fdc7e8f7695b14c52a73bb38d
Simon Godik <simon@godik.com>
2025-01-12 02:57:29 +0000

    pass image pull secret name to the operator

--- install-kafka-operator.sh
+++ install-kafka-operator.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+# install, upgrade
+helm_action=${1:-"install"}
+INSTANA_VERSION_OVERRIDE=$2
+
 source ../instana.env
 source ./help-functions.sh
+source ./datastore-images.env
 
 CHART_HOME=$(get_chart_home)
 
-CHART=$CHART_HOME/strimzi-kafka-operator-helm-3-chart-0.41.0.tgz
+CHART=$CHART_HOME/strimzi-kafka-operator-helm-3-chart-${KAFKA_OPERATOR_CHART_VERSION}.tgz
 
 echo installing kafka operator helm chart $CHART
 
@@ -14,12 +19,18 @@ if test ! -f $CHART; then
    exit 1
 fi
 
-helm install kafka-operator -n instana-kafka $CHART \
+set -x
+
+kafka_operator_img_tag=`echo ${KAFKA_OPERATOR_IMG} | cut -d : -f 2 -`
+kafka_img_tag=`echo ${KAFKA_IMG} | cut -d : -f 2 -`
+
+helm ${helm_action} kafka-operator -n instana-kafka $CHART \
    --set image.registry=$PRIVATE_REGISTRY  \
    --set image.repository=self-hosted-images/3rd-party/operator \
    --set image.name=strimzi  \
-   --set image.tag=0.41.0_v0.9.0 \
+   --set image.tag=${kafka_operator_img_tag} \
    --set kafka.image.registry=$PRIVATE_REGISTRY \
    --set kafka.image.repository=self-hosted-images/3rd-party/datastore \
    --set kafka.image.name=kafka \
-   --set kafka.image.tag=0.41.0-kafka-3.6.2_v0.7.0
+   --set kafka.image.tag=${kafka_img_tag} \
+   --set image.imagePullSecrets[0].name="instana-registry"
