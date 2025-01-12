2bf737db962105286c268bbfd5cf58fe4cdb64f1
Simon Godik <simon@godik.com>
2025-01-12 08:23:36 +0000

    clickhouse cr patch

---
+++ cr-clickhouse-patch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+INSTANA_VERSION_OVERRIDE=$2
+
+source ../instana.env
+source ./help-functions.sh
+source ./datastore-images.env
+
+OUT_DIR=$(get_make_manifest_home)
+MANIFEST="$OUT_DIR/clickhouse-patch-${INSTANA_VERSION}.yaml"
+
+replace_manifest=${1:-"noreplace"}
+
+check_replace_manifest $MANIFEST $replace_manifest
+
+echo writing clickhouse patch to $MANIFEST
+
+cat <<EOF > $MANIFEST
+spec:
+  templates:
+    podTemplates:
+    - name: clickhouse
+      spec:
+        containers:
+
+        - name: instana-clickhouse
+          image: ${PRIVATE_REGISTRY}/${CLICKHOUSE_OPENSSL_IMG}
+        - name: clickhouse-log
+          image: ${PRIVATE_REGISTRY}/${CLICKHOUSE_OPENSSL_IMG}
+EOF
---
+++ install-clickhouse-apply-patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INSTANA_VERSION_OVERRIDE=$1
+
+source ../instana.env
+source ./help-functions.sh
+
+MANIFEST_HOME=$(get_manifest_home)
+
+MANIFEST=$MANIFEST_HOME/clickhouse-patch-${INSTANA_VERSION}.yaml
+
+SNAPSHOT_HOME=$(get_make_snapshot_home)
+
+SNAPSHOT=${SNAPSHOT_HOME}/clickhouse-snapshot-`date +%F-%H-%M-%S`.yaml
+
+echo applying clickhouse patch $MANIFEST, namespace instana-clickhouse
+
+if test ! -f $MANIFEST; then
+   echo manifest $MANIFEST not found
+   exit 1
+fi
+
+set -x
+
+$KUBECTL get chi/instana -n instana-clickhouse -o yaml > ${SNAPSHOT}
+
+$KUBECTL patch chi/instana --type merge --patch-file ${MANIFEST} -n instana-clickhouse
