beb3f1be961f63dac84c7a14e573a33c22cd78f2
Simon Godik <simon@godik.com>
2025-01-12 07:04:09 +0000

    postgres cr patch

---
+++ cr-postgres-patch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+INSTANA_VERSION_OVERRIDE=$2
+
+source ../instana.env
+source ./help-functions.sh
+source ./datastore-images.env
+
+OUT_DIR=$(get_make_manifest_home)
+MANIFEST="$OUT_DIR/postgres-patch-${INSTANA_VERSION}.yaml"
+
+replace_manifest=${1:-"noreplace"}
+
+check_replace_manifest $MANIFEST $replace_manifest
+
+echo writing postgres patch to $MANIFEST
+
+cat <<EOF > $MANIFEST
+spec:
+  imageName: ${PRIVATE_REGISTRY}/${POSTGRES_IMG}
+EOF
---
+++ install-postgres-apply-patch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+INSTANA_VERSION_OVERRIDE=$1
+
+source ../instana.env
+source ./help-functions.sh
+
+MANIFEST_HOME=$(get_manifest_home)
+
+MANIFEST=$MANIFEST_HOME/postgres-patch-${INSTANA_VERSION}.yaml
+
+SNAPSHOT_HOME=$(get_make_snapshot_home)
+
+SNAPSHOT=${SNAPSHOT_HOME}/postgres-snapshot-`date +%F-%H-%M-%S`.yaml
+
+echo applying postgres patch $MANIFEST, namespace instana-postgres
+
+if test ! -f $MANIFEST; then
+   echo manifest $MANIFEST not found
+   exit 1
+fi
+
+set -x
+
+$KUBECTL get cluster/postgres -n instana-postgres -o yaml > ${SNAPSHOT}
+
+$KUBECTL patch cluster/postgres --type merge --patch-file ${MANIFEST} -n instana-postgres
