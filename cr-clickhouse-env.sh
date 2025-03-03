d331f1e160b9d9f2f4fde7c3be35fc0506550aab
Simon Godik <simon@godik.com>
2025-03-03 21:10:12 +0000

    env templates updates

--- cr-clickhouse-env.sh
+++ cr-clickhouse-env.sh
@@ -5,6 +5,8 @@ source ./datastore-images.env
 source ./help-functions.sh
 source ./cr-env.sh
 
+replace_manifest=${1:-"noreplace"}
+
 export clickhouse_container_image=${PRIVATE_REGISTRY}/${CLICKHOUSE_OPENSSL_IMG}
 export clickhouse_log_image=${PRIVATE_REGISTRY}/${CLICKHOUSE_OPENSSL_IMG}
 
@@ -15,9 +17,14 @@ export clickhouse_user_pass=${CLICKHOUSE_USER_PASS}
 
 template_cr="clickhouse-template.yaml"
 env_file="clickhouse-env.yaml"
-out_file="gen/clickhouse-env-289.yaml"
 profile=${INSTANA_INSTALL_PROFILE:-"template"}
 
-cp $template_cr $out_file
+OUT_DIR=$(get_make_manifest_home)
+MANIFEST="${OUT_DIR}/clickhouse-env-${INSTANA_VERSION}.yaml"
 
-cr_env $template_cr $env_file $out_file $profile
+check_replace_manifest $MANIFEST $replace_manifest
+cp $template_cr $MANIFEST
+
+cr_env $template_cr $env_file $MANIFEST $profile
+
+echo updated clickhouse manifest $MANIFEST
