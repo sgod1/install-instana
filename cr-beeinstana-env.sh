ef80fa720eae1e649f8d68f757fb3fbb6d224420
Simon Godik <simon@godik.com>
2025-03-03 21:42:05 +0000

    cr env copy manifest

--- cr-beeinstana-env.sh
+++ cr-beeinstana-env.sh
@@ -5,11 +5,18 @@ source ./datastore-images.env
 source ./help-functions.sh
 source ./cr-env.sh
 
+replace_manifest=${1:-"noreplace"}
+
 template_cr="beeinstana-template.yaml"
 env_file="beeinstana-env.yaml"
-out_file="gen/beeinstana-env-289.yaml"
 profile=${INSTANA_INSTALL_PROFILE:-"template"}
 
-cp $template_cr $out_file
+OUT_DIR=$(get_make_manifest_home)
+MANIFEST="${OUT_DIR}/beeinstana-env-${INSTANA_VERSION}.yaml"
 
-cr_env $template_cr $env_file $out_file $profile
+check_replace_manifest $MANIFEST $replace_manifest
+cp $template_cr $MANIFEST
+
+cr_env $template_cr $env_file $MANIFEST $profile
+
+echo updated beeinstana manifest $MANIFEST
--- cr-kafka-env.sh
+++ cr-kafka-env.sh
@@ -5,11 +5,18 @@ source ./datastore-images.env
 source ./help-functions.sh
 source ./cr-env.sh
 
+replace_manifest=${1:-"noreplace"}
+
 template_cr="kafka-template.yaml"
 env_file="kafka-env.yaml"
-out_file="gen/kafka-env-289.yaml"
 profile=${INSTANA_INSTALL_PROFILE:-"template"}
 
-cp $template_cr $out_file
+OUT_DIR=$(get_make_manifest_home)
+MANIFEST="${OUT_DIR}/kafka-env-${INSTANA_VERSION}.yaml"
 
-cr_env $template_cr $env_file $out_file $profile
+check_replace_manifest $MANIFEST $replace_manifest
+cp $template_cr $MANIFEST
+
+cr_env $template_cr $env_file $MANIFEST $profile
+
+echo updated kafka manifest $MANIFEST
