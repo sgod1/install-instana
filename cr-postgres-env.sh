9f8456a7f45f62934df04486967ebd33b45061ce
Simon Godik <simon@godik.com>
2025-03-12 18:41:27 +0000

    postgres env settings

--- cr-postgres-env.sh
+++ cr-postgres-env.sh
@@ -7,6 +7,9 @@ source ./cr-env.sh
 
 replace_manifest=${1:-"noreplace"}
 
+export postgres_image=${PRIVATE_REGISTRY}/${POSTGRES_IMG}
+export rwo_storageclass=${RWO_STORAGECLASS}
+
 template_cr="postgres-template.yaml"
 env_file="postgres-env.yaml"
 profile=${INSTANA_INSTALL_PROFILE:-"template"}
@@ -15,8 +18,8 @@ OUT_DIR=$(get_make_manifest_home)
 MANIFEST="${OUT_DIR}/postgres-env-${INSTANA_VERSION}.yaml"
 
 check_replace_manifest $MANIFEST $replace_manifest
-cp $template_cr $MANIFEST
+copy_template_manifest $template_cr $MANIFEST $profile
 
 cr_env $template_cr $env_file $MANIFEST $profile
 
-echo updated postgres manifest $MANIFEST
+echo updated postgres manifest $MANIFEST, profile $profile
