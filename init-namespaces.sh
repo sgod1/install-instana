371249378f8729fe39b25738fb68b6d252e491b4
Simon Godik <simon@godik.com>
2025-03-24 00:17:44 +0000

    kubectl wait --for

--- init-namespaces.sh
+++ init-namespaces.sh
@@ -8,17 +8,13 @@ select_label=${2:-""}
 
 INSTALL_HOME=$(get_install_home)
 
-function create_psa_labeled_namespace() {
+function create_namespace() {
   local ns=$1
-  local label=$2
+  local psa_label=$2
+
+  echo ... create namespace $ns, psa label ${psa_label:-"no label"}, platform $PLATFORM
 
   NS_YAML="$INSTALL_HOME/ns-$ns.yaml"
-
-  if ! is_platform_ocp "$PLATFORM"; then
-     echo creating namespace $ns, label "pod-security.kubernetes.io/enforce:" $label
-  else
-     echo creating namespace $ns
-  fi
 
   # write out namespace
 cat << EOF > $NS_YAML
@@ -27,95 +23,85 @@ kind: Namespace
 metadata:
   name: $ns
 
+  labels:
+    app.kubernetes.io/name: $ns
+
 EOF
 
-  # write out psa label
-  if ! is_platform_ocp "$PLATFORM"; then
+  if test $psa_label; then
 cat << EOF >> $NS_YAML
-  labels:
     #
     # set admission-control mode (enforce) and pss level (privileged/baseline/restricted) for pod security
     # psa will check pod request for conformance with pss level
     #
-    pod-security.kubernetes.io/enforce: $label
+    pod-security.kubernetes.io/enforce: $psa_label
     pod-security.kubernetes.io/enforce-version: latest
 EOF
   fi
 
-  $KUBECTL apply -f $NS_YAML; rc=$?; check_return_code $rc
-  rm $NS_YAML
-}
+  $KUBECTL apply -f $NS_YAML
+  check_return_code $?
 
-function create_labeled_namespace() {
-  local ns=$1
-  local label=$2
+  $KUBECTL wait --for=jsonpath='{.status.phase}'=Active ns/$ns
+  check_return_code $?
 
-  echo creating namespace $ns, label "app.kubernetes.io/name:" $label
-
-  NS_YAML="$INSTALL_HOME/ns-$ns.yaml"
-
-cat << EOF > $NS_YAML
-apiVersion: v1
-kind: Namespace
-metadata:
-  name: $ns
-  labels:
-    app.kubernetes.io/name: $ns
-EOF
-
-  $KUBECTL apply -f $NS_YAML; rc=$?; check_return_code $rc
-  rm $NS_YAML
-}
-
-function create_namespace() {
-  local ns=$1
-  local label=$2
-
-  if compare_values "$label" "privileged"; then
-    create_psa_labeled_namespace $ns $label
-
-  elif compare_values "$label" "baseline"; then
-    create_psa_labeled_namespace $ns $label
-
-  elif compare_values "$label" "restricted"; then
-    create_psa_labeled_namespace $ns $label
-
-  elif test ! -z "$label"; then
-    create_labeled_namespace $ns $label
-
-  else
-    echo creating namespace $ns
-    $KUBECTL create namespace $ns; rc=$?; check_return_code $rc
-  fi
+  echo ... namespace $ns status Active ...
 }
 
 function create_image_pull_secret() {
    local ns=$1
    local secret_name="instana-registry"
+   local rc
 
-   echo creating image pull secret $secret_name, namespace $ns
+   echo ... get image pull secret $secret_name, namespace $ns
+   $KUBECTL get secret $secret_name --namespace $ns 2>/dev/null
+   rc=$?
 
+   if [[ $rc == 0 ]]; then
+      echo ... deleting image pull secret $secret_name, namespace $ns
+      $KUBECTL delete secret $secret_name --namespace $ns; rc=$?
+      check_return_code $rc
+
+      #$KUBECTL wait --for=delete secret/$secret_name --namespace $ns
+      #check_return_code $rc
+   fi
+
+   echo ... creating image pull secret $secret_name, namespace $ns
    $KUBECTL create secret docker-registry $secret_name \
       --namespace $ns \
       --docker-username=$PRIVATE_REGISTRY_USER \
       --docker-password=$PRIVATE_REGISTRY_PASSWORD \
-      --docker-server=$PRIVATE_DOCKER_SERVER; rc=$?; check_return_code $rc
+      --docker-server=$PRIVATE_DOCKER_SERVER;
+   rc=$?
+   check_return_code $rc
+
+   #$KUBECTL wait --for=create secret/$secret_name --namespace $ns
+   #check_return_code $rc
 }
 
 function init_namespace() {
    local ns=$1
    local label=$2
+
+   echo
+   echo ... namespace $ns, label ${label:-"no label"}
+
    create_namespace $ns $label
    create_image_pull_secret $ns
 }
 
+#
+# main
+#
+
 echo
-echo initializing namespaces...
+echo ... initializing namespaces
 echo
 
 if test ! -z $select_namespace; then
 init_namespace $select_namespace $select_label
 else
+
 init_namespace "cert-manager" ${K8S_PSA_LABEL:-"privileged"}
 init_namespace "instana-zookeeper" ${K8S_PSA_LABEL:-"privileged"}
 init_namespace "instana-kafka" ${K8S_PSA_LABEL:-"privileged"}
@@ -125,7 +111,12 @@ init_namespace "instana-cassandra" ${K8S_PSA_LABEL:-"privileged"}
 init_namespace "instana-clickhouse"
 init_namespace "beeinstana"
 init_namespace "instana-operator"
-init_namespace "instana-core" "instana-core"
-init_namespace "instana-units" "instana-units"
+init_namespace "instana-core"
+init_namespace "instana-units"
+
+echo
+echo ... namespaces initialized
+echo
+
 fi
 
